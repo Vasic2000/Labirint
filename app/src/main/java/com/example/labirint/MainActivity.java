e1be56131cb9e26c3918b11da9eae7efb815b974
Vasic2000 <Vasic2000@yandex.ru>
2019-10-09 11:27:11 +0300

    Проба рисовать

--- app/src/main/java/com/example/labirint/MainActivity.java
+++ app/src/main/java/com/example/labirint/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.labirint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private Paint paint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(new MyView(this));
+    }
+
+    class MyView extends View {
+
+        public MyView(Context context) {
+            super(context);
+            paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            paint.setColor(Color.RED);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawColor(Color.GREEN);
+            canvas.drawCircle(150,150,100,paint);
+        }
     }
 }
